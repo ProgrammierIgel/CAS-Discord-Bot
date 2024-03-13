d2dc3ea92e2fead74e6f9ed499c5d299b34361f2
ProgrammierIgel <144588240+ProgrammierIgel@users.noreply.github.com>
2024-03-13 19:35:18 +0100

    change token

--- BOT.py
+++ BOT.py
@@ -15,4 +15,4 @@ class MyClient(discord.Client):
 intents = discord.Intents.default()
 intents.message_content = True
 client = MyClient(intents=intents)
-client.run('MTIxNzUxNDY0NzMxODQyOTc3Ng.GhZxIC.u2IizstWsI5pJIM3kUJpu_aVFFlgDu9bDcs-eg')
+client.run('123')
